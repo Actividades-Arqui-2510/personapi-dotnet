010fa1e07f70cbbbe7a480f1787c71e0de7b2e7d
CaMoraG <camoraq83@gmail.com>
2025-04-08 21:31:36 -0500

    Initial commit

---
+++ personapi-dotnet/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace personapi_dotnet.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
