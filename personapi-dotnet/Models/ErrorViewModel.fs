7782074eaad7ecbca3abfdc71c564c137a1b48b3
CaMoraG <camoraq83@gmail.com>
2025-04-09 18:21:41 -0500

    Change to C#

--- personapi-dotnet/Models/ErrorViewModel.fs
+++
@@ -1,9 +0,0 @@
-namespace personapi_dotnet.Models
-
-open System
-
-type ErrorViewModel =
-    { RequestId: string }
-
-    member this.ShowRequestId =
-        not (String.IsNullOrEmpty(this.RequestId))
